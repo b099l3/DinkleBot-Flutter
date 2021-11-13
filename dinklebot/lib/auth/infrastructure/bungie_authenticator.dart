57109caec68e73bb704f22ce52164b1edfaf3c43
Iain Smith <b099l3@gmail.com>
2021-11-13 22:01:16 +0000

    auth code minus all the secrets

---
+++ dinklebot/lib/auth/infrastructure/bungie_authenticator.dart
@@ -0,0 +1,131 @@
+import 'package:dartz/dartz.dart';
+import 'package:dinklebot/auth/domain/auth_failure.dart';
+import 'package:dinklebot/auth/infrastructure/credentials_storage/credentials_storage.dart';
+import 'package:dinklebot/core/infrastructure/dio_extensions.dart';
+import 'package:dinklebot/core/shared/encoders.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'package:oauth2/oauth2.dart';
+
+class BungieOAthHttpClient extends http.BaseClient {
+  final httpClient = http.Client();
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['Accept'] = 'application/json';
+    return httpClient.send(request);
+  }
+}
+
+class BungieAuthenticator {
+  final CredentialsStorage _credentialsStorage;
+  final Dio _dio;
+
+  BungieAuthenticator(this._credentialsStorage, this._dio);
+
+  Future<Credentials?> getSignedInCredentials() async {
+    try {
+      final storedCredentials = await _credentialsStorage.read();
+      if (storedCredentials != null) {
+        if (storedCredentials.canRefresh && storedCredentials.isExpired) {
+          final failureOrCredentials = await refresh(storedCredentials);
+          return failureOrCredentials.fold((l) => null, (r) => r);
+        }
+      }
+      return storedCredentials;
+    } on PlatformException {
+      return null;
+    }
+  }
+
+  Future<bool> isSignedIn() =>
+      getSignedInCredentials().then((credentials) => credentials != null);
+
+  AuthorizationCodeGrant createGrant() {
+    return AuthorizationCodeGrant(
+      clientId,
+      authorisationEndpoint,
+      tokenEndpoint,
+      secret: clientSecret,
+      httpClient: BungieOAthHttpClient(),
+    );
+  }
+
+  Uri getAuthorisationUrl(AuthorizationCodeGrant grant) {
+    return grant.getAuthorizationUrl(redirectUrl, scopes: scopes);
+  }
+
+  Future<Either<AuthFailure, Unit>> handleAuthorisationResponse(
+    AuthorizationCodeGrant grant,
+    Map<String, String> queryParameters,
+  ) async {
+    try {
+      final httpClient =
+          await grant.handleAuthorizationResponse(queryParameters);
+      await _credentialsStorage.save(httpClient.credentials);
+      return right(unit);
+    } on FormatException catch (e) {
+      return left(AuthFailure.server('Format Exception: ${e.message}'));
+    } on AuthorizationException catch (e) {
+      return left(AuthFailure.server('${e.error}: ${e.description}'));
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
+
+  Future<Either<AuthFailure, Unit>> signOut() async {
+    final accessToken = await _credentialsStorage
+        .read()
+        .then((credentials) => credentials?.accessToken);
+
+    final usernameAndPassword =
+        stringToBase64.encode('$clientId:$clientSecret');
+
+    try {
+      try {
+        _dio.deleteUri(
+          revocationEndpoint,
+          data: {
+            'access_token': accessToken,
+          },
+          options: Options(
+            headers: {
+              'authorisation': 'basic $usernameAndPassword',
+            },
+          ),
+        );
+      } on DioError catch (e) {
+        if (e.isNoConnectionError) {
+          // ignoring
+        } else {
+          rethrow;
+        }
+      }
+      await _credentialsStorage.clear();
+      return right(unit);
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
+
+  Future<Either<AuthFailure, Credentials>> refresh(
+    Credentials credentials,
+  ) async {
+    try {
+      final refreshedCredentials = await credentials.refresh(
+        identifier: clientId,
+        secret: clientSecret,
+        httpClient: BungieOAthHttpClient(),
+      );
+      await _credentialsStorage.save(refreshedCredentials);
+      return right(refreshedCredentials);
+    } on FormatException {
+      return left(const AuthFailure.server("Format Exception"));
+    } on AuthorizationException catch (e) {
+      return left(AuthFailure.server("${e.error}: ${e.description}"));
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
+}
